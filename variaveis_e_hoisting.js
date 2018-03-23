2a686e61b98fdca870ff86c338d7f2dcee0c5d7e
JonesQuito <jonesdhy@hotmail.com>
2018-03-22 22:31:49 -0300

    variáveis e hoisting

---
+++ variaveis_e_hoisting.js
@@ -0,0 +1,10 @@
+var percentualImposto = 0.15;
+var valor = 100;
+var totalImposto = valor * percentualImposto;
+
+console.log('Total imposto a pagar:', totalImposto);
+
+//HOISTING
+/*Antecipa a declaração de todas as variáveis
+encontradas no arquivo e só depois segue o fluxo normal
+da aplicação*/
